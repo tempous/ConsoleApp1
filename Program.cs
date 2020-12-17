5ad63bbd95815d755a70c4cfb16d0b6e41fe83fc
igzotix <igzotix@inbox.ru>
2020-12-17 21:56:25 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число:");
+            int count = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Index: {i} - Value: {i+1}");
+            }
+            Console.WriteLine("Finish...");
+            Console.ReadKey();
+        }
+    }
+}
